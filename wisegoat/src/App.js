7457ed062343157e1d7be30a3b3c4d7ae2c08b96
Jessica Latorre <jessica.latorre182@gmail.com>
2021-05-14 00:15:01 -0300

    conclui a página home

--- wisegoat/src/App.js
+++ wisegoat/src/App.js
@@ -1,44 +1,84 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { keyframes }  from 'styled-components'
+
 import ConselhoBode from './components/ConselhoBode'
 import bodezinhoGuru from './assets/bodezinho.png'
 
+
+const BodeGuru = keyframes`
+0% {
+  transform: translatey(-30px)
+}
+50%{
+  transform: translatey(30px)
+}
+100%{
+  transform: translatey(-30px)
+}
+`
 const TelaPrincipal = styled.div`
-
 height: 100vh;
-display: flex;
-justify-content: center;
+display:flex;
 align-items: center;
+justify-content: center;
 `
-const WiseGoat = styled.h1`
-color: #BAA193;
-font-family:;
+const Container = styled.div`
+height: 80vh;
+width: 30vw;
+display: flex;
+flex-direction: column;
+justify-content: center;
 `
-
-const ButtonPrincipal = styled.button`
-background-color: #72005F;
-border-radius: 8px;
-width: 20%;
+const Wise = styled.h2`
+color: #bdb6a4;
+width:20vw;
+font-size: 48px;
+margin-bottom: 0px;
+`
+const Goat = styled.h1`
+width:20vw;
+color: #720057;
+font-size: 120px;
+margin-bottom: 10px;
+margin-top: 0px;
+`
+const Button = styled.button`
+width: 20vw;
+color: white;
+border: none;
+border-radius: 10px;
+font-size:26px;
+padding: 16px 22px;
+background-color: #72005f;
+margin-top: 55px;
 
 `
-
-const ImagemBode = styled.img`
-border: solid green 2px;
-height: 50%;
-
-
-
+const Bode = styled.img`
+width: 25%;
+height:78vh;
+margin-bottom:100px;
+animation: ${BodeGuru};
+animation-duration: 2s;
+animation-timing-function: ease-in-out;
+animation-iteration-count: infinite;
 `
-
-
 function App() {
   return (
   
     <TelaPrincipal>
-      <WiseGoat>Wise</WiseGoat>
-      <ButtonPrincipal>Pedir Conselho</ButtonPrincipal>
-        <ImagemBode src={bodezinhoGuru}/>
+      <Container>
+        <Wise>
+          Wise
+        </Wise>
+        <Goat>
+          Goat
+        </Goat>
       
+        <Button>Pedir conselho</Button>
+      
+      </Container>
+      <Bode src={bodezinhoGuru}></Bode>
+    
     </TelaPrincipal>
     
   );
--- wisegoat/src/components/ConselhoBode.js
+++ wisegoat/src/components/ConselhoBode.js
@@ -1,15 +1,14 @@
+
+import React from 'react'
 import styled from 'styled-components'
 
 
-const ButtonPrincipal = styled.button`
-background-color: #610085;
-border-radius: 8px;
-width: 36px;
-`
+
+
 
 function ConselhoBode () {
     return (
-        <ButtonPrincipal>Pedir conselho</ButtonPrincipal>
+        <button>Pedir conselho</button>
     )
 }
 
